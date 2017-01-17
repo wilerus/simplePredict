990b5d9947f77c1330e21950169c1282e1da9b88
Unknown <negnichelovek@gmail.com>
2017-01-17 21:40:10 +0300

    Update image crop

--- simpleJS3/Scripts/Application.js
+++ simpleJS3/Scripts/Application.js
@@ -39,45 +39,57 @@ class MainApplication extends HTMLElement {
 <application-input><application-input>
 </div>`;
 
-      document.addEventListener('canvas:received:drawData', e => this.recognizeImage(e.detail), false);
+        document.addEventListener('canvas:received:drawData', e => this.recognizeImage(e.detail), false);
     }
 
     recognizeImage(drawData) {
-      const reducedImageData = this.reduceImage(drawData);
-      //this.predictionsModule.recognizeImage(reducedImageData);
+        const reducedImageData = this.reduceImage(drawData);
+        //this.predictionsModule.recognizeImage(reducedImageData);
     }
 
     reduceImage(drawData) {
-      const croppedDrawData = this.removeEmptyColumnAndRows(drawData);
-      return this.interpolateImageData(croppedDrawData);
+        const croppedDrawData = this.removeEmptyColumnAndRows(drawData);
+        const interpolatedImageData = this.interpolateImageData(croppedDrawData);
+        return this.normalizeImageData(interpolatedImageData);
     }
 
     removeEmptyColumnAndRows(drawData) {
-      const emptyRowsData = this.calculateEmptyRowsAndColumn(drawData);
-      return this.cropImage(drawData, emptyRowsData);
+        const emptyRowsData = this.calculateEmptyRowsAndColumn(drawData);
+        return this.cropImage(drawData, emptyRowsData);
     }
 
     interpolateImageData(imageData) {
-       const pixelmap = imageData.pixelmap;
-       const imageDimentions = imageData.dimentions;
+        const pixelmap = imageData.pixelmap;
+        const imageDimentions = imageData.dimentions;
     }
 
     calculateEmptyRowsAndColumn(imageData) {
         const pixelmap = imageData.data;
         const imageDimentionsWidth = imageData.width;
         let freeRowsFromLeft = imageDimentionsWidth;
+        let isRowEmpty = true;
         let freeRowsFromRight = 0;
-        let freeColumnsFromTop = 0;
+        let freeColumnsFromTop = -1;
         let freeColumnsFromBottom = 0;
+        let oldColumnPosition = 0;
 
         for (let pixelMapPosition = 0; pixelMapPosition < pixelmap.length; pixelMapPosition+=4) {
             const columnPosition = Math.floor(pixelMapPosition / imageDimentionsWidth);
-            if (this.__isFilled(pixelmap.slice(pixelMapPosition, pixelMapPosition + 4))) {
-                if (pixelMapPosition - columnPosition * imageDimentionsWidth < freeRowsFromLeft) {
-                    freeRowsFromLeft = pixelMapPosition - columnPosition * imageDimentionsWidth;
+            if (oldColumnPosition !== columnPosition) {
+                if (isRowEmpty) {
+                    freeColumnsFromTop = oldColumnPosition;
                 }
-                if (pixelMapPosition - columnPosition * imageDimentionsWidth > freeRowsFromRight) {
-                    freeRowsFromRight = pixelMapPosition - columnPosition * imageDimentionsWidth;
+                oldColumnPosition = columnPosition;
+                isRowEmpty = true;
+            }
+            if (this.__isFilled(pixelmap.slice(pixelMapPosition, pixelMapPosition + 4))) {
+                isRowEmpty = false;
+                const matrixPosition = pixelMapPosition - columnPosition * imageDimentionsWidth;
+                if (matrixPosition < freeRowsFromLeft) { //todo fix one row slap
+                    freeRowsFromLeft = matrixPosition;
+                }
+                if (matrixPosition > freeRowsFromRight) {
+                    freeRowsFromRight = matrixPosition;
                 }
             }
         }
@@ -87,6 +99,7 @@ class MainApplication extends HTMLElement {
     cropImage(imageData, emptyRowsData) {
         const pixelmap = imageData.data;
         const imageDimentionsWidth = imageData.width;
+
         return pixelmap.filter((value, index) => {
             let currentPosition = Math.floor(index / 4);
             let columnPosition = Math.floor(index / imageDimentionsWidth);
@@ -96,6 +109,10 @@ class MainApplication extends HTMLElement {
 
     __isFilled(pixel) {
         return pixel.some(property => property !== 0);
+    }
+
+    normalizeImageData() {
+
     }
 };
 customElements.define('main-application', MainApplication);
